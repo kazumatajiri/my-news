ccdeebde99375ffb78190a00867cbf7ac584546f
kazuma <kazuma910abc@icloud.com>
2020-06-27 19:44:03 +0000

    (laravel 11)

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>kadai 1</title>
-    </head>
-    <body>
-        <h1>課題１</h1>
-    </body>
-</html>
+@section('title', 'My プロフィール')
+
+@section('content')
+  <div class="container">
+     <div class="row">
+        <div class="col-md-8 mx-auto">
+           <h2>My プロフィール</h2>
+        </div>
+     </div>
+  </div>
+@endsection
