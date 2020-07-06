00c4b472f8af324956fa0ffccd4a1e064fa6cd30
kazuma <kazuma910abc@icloud.com>
2020-07-06 10:24:31 +0000

    (laravel 12)

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create','Admin\NewsController@add');
-});
 
 
-Route::group(['prefix'=>'admin'], function(){
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
+
